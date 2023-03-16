d5860837d349298fc92d640a48d96731b9ee7c6f
leoemaxie <leoemaxie@gmail.com>
2023-03-16 11:33:45 -0700

    Corrections to advanced tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Description: A C program that prints to the stderr through the fprintf
- * function
+ * Description: A C program that prints to the stderr
  *
- * Return: 0 on success
+ * Return: 1 on success
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - 
-			Dora Korpar, 2015-10-19\n");
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
