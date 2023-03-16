0d1714c2126b03b497033b987cdc522cf1df7ffe
Emmanuel Lafenwa <103007301+leoemaxie@users.noreply.github.com>
2023-03-16 18:08:02 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,26 @@
-#include <stdio.h>
- int main(void)
-{
-	return(0);
-}
+#include <stdio.h> 
+  
+ /** 
+  *  main - Entry point 
+  *
+  *  Description: Program to display the
+  *  size of C data types
+  *
+  *  Return: 0 on success
+  * 
+  */ 
+ int main(void) 
+ { 
+                 int a; 
+                 long int b; 
+                 long long int c; 
+                 char d; 
+                 float f; 
+  
+                 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d)); 
+                 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a)); 
+                 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b)); 
+                 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c)); 
+                 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f)); 
+                 return (0); 
+ }
