2f376d112c910b599ca70ddf668ffbc27d6d6487
leoemaxie <leoemaxie@gmail.com>
2023-03-16 07:06:18 -0700

    Added some basic task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
- int main(void)
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints to the stdout through the puts function
+ *
+ * Return: 0 on success
+ */
+int main(void)
 {
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
- int main(void)
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints to the stdout through the printf function
+ *
+ * Return: 0 on success
+ */
+int main(void)
 {
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
