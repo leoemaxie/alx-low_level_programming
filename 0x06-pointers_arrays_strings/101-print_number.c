86848b766c09cecd13f4243390b6647a70cd3487
leoemaxie <leoemaxie@gmail.com>
2023-03-30 20:32:43 +0100

    Fixed some advanced tasks

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_number - Prints an integer to stdout.
  *
@@ -9,18 +9,13 @@
  */
 void print_number(int n)
 {
-	int rem;
-
 	if (n < 0)
 	{
 		n = -n; /* Converts n to a positive number to prevent a leading minus */
-		putchar(45); /* ascii code for - */
+		_putchar(45); /* ascii code for - */
 	}
 
-	while (n != 0)
-	{
-		rem = n % 10;
-		putchar(rem + 48);
-		n = n / 10;
-	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar((n % 10) + 48);
 }
