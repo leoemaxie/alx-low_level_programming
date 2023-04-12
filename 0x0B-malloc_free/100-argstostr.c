a325bb928de57db2d25700fd2d1b15aab1ef7780
leoemaxie <leoemaxie@gmail.com>
2023-04-12 02:12:38 +0100

    Fix failing checker

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,7 +11,7 @@
 char *argstostr(int ac, char **av)
 {
 	int i, j, k = 0;
-	char *str = malloc(sizeof(int *) * ac);
+	char *str = malloc(sizeof(int *) * ac - 1);
 
 	if (str == NULL || ac == 0 || av == NULL)
 		return (NULL);
@@ -22,7 +22,7 @@ char *argstostr(int ac, char **av)
 			str[k] = av[i][j];
 
 		str[k] = '\n';
-		k++;
+		k++
 	}
 
 	return (str);
