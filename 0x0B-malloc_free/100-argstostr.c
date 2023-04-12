1d45aa6d30885fa45ae78e79cfa55d3c8b57a927
leoemaxie <leoemaxie@gmail.com>
2023-04-12 04:04:02 +0100

    Fix failing checker emergency

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,7 +11,7 @@
 char *argstostr(int ac, char **av)
 {
 	int i, j, k = 0;
-	char *str = malloc(sizeof(int *) * ac - 1);
+	char *str = malloc(sizeof(char) * ac + 1);
 
 	if (str == NULL || ac == 0 || av == NULL)
 		return (NULL);
@@ -21,7 +21,7 @@ char *argstostr(int ac, char **av)
 		for (j = 0; av[i][j] != '\0'; j++, k++)
 			str[k] = av[i][j];
 
-		str[k] = '\n';
+		str[k++] = '\n';
 		k++;
 	}
 
