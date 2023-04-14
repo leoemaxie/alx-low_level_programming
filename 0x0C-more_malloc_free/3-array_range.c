c8e4c8afbb324ce86c0c0bda3fcdf4b9d808c03b
leoemaxie <leoemaxie@gmail.com>
2023-04-14 01:25:24 +0100

    Fixed some issues

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,12 +12,16 @@ int *array_range(int min, int max)
 {
 	int i;
 	int size = max - min + 1;
-	int *array = (size > 0) ? malloc(sizeof(int) * (size)) : NULL;
+	int *array;
 
+	if (min > max)
+		return (NULL);
+
+	array = malloc(sizeof(int) * (size));
 	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i <= size; i++, min++)
+	for (i = 0; min <= max; min++)
 		array[i] = min;
 
 	return (array);
