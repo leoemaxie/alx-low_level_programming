d33a3237bdfab89048275ac5491235ffac3559d1
leoemaxie <leoemaxie@gmail.com>
2023-04-14 01:34:54 +0100

    Fixed some issues

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,7 +27,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; i < len_s1; i++)
+	for (i = 0; i <= len_s1; i++)
 		str[i] = s1[i];
 	for (i = 0; i <= n; i++)
 	{
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			break;
 		str[len_s1 + i] = s2[i];
 	}
-	str[len_s1 + n] = '\0';
+	str[len_s1 + i] = '\0';
 
 	return (str);
 }
