1070df780eb05016e6ead45706b99366300c1986
leoemaxie <leoemaxie@gmail.com>
2023-04-17 18:06:41 +0100

    Fix Issues - 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,11 +13,12 @@ struct dog {
 	char *owner;
 };
 
-typedef struct dog dog_t;
-
 /* function declarations */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
