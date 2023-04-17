26df6f38cf90cef1b8f347597a59581a10f4ada6
leoemaxie <leoemaxie@gmail.com>
2023-04-17 18:10:10 +0100

    Fix Issues - 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,19 +6,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* Structures */
+/**
+ * struct dog - Dog Class.
+ *
+ * @name: Name of the dog.
+ *
+ * @age: Age of the dog.
+ *
+ * @owner: Name of the dog's owner.
+ */
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 /* function declarations */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-typedef struct dog dog_t;
-
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
