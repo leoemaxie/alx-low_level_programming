2f470c91dfc6b55941d5b543dbf604b9c90c79f5
leoemaxie <leoemaxie@gmail.com>
2023-04-17 18:23:05 +0100

    Handled NUll

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog = malloc(sizeof(dog_t));
 
-	if (dog == NULL)
+	if (dog == NULL || name == NULL || owner == NULL)
 		return (NULL);
 
 	dog->name = strdup(name);
