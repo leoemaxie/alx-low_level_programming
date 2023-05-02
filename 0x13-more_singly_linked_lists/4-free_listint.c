8bf3853543786d14e089c2903d88165114cad6b8
leoemaxie <leoemaxie@gmail.com>
2023-05-02 17:53:32 +0100

    Memory fixes

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,12 +4,15 @@
  * free_listint - Frees a listint_t list.
  *
  * @head: Pointer to listint_t list.
- *
  */
 void free_listint(listint_t *head)
 {
-	listint_t *i;
+	listint_t *next;
 
-	for (i = head; i != NULL; i = i->next)
-		free(i);
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
 }
