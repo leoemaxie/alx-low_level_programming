89076eb499b88ff478f817f9e14b8245bf4551ac
leoemaxie <leoemaxie@gmail.com>
2023-05-09 05:06:07 +0100

    Added advamced tasks

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file = filename ? open(filename,  O_RDWR | O_APPEND) : -1;
+	int file = filename ? open(filename,  O_WRONLY | O_APPEND) : -1;
 
 	if (file == -1)
 		return (-1);
