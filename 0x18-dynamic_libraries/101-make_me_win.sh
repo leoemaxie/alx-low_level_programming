a5146209bad3e66a4f7aa8c91bac7252ef76f510
leoemaxie <leoemaxie@gmail.com>
2023-08-22 03:52:36 +0100

    Minor fixes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/leoemaxie/alx-low_level_programming/raw/main/0x18-dynamic_libraries/gm.so
+wget -P /tmp https://github.com/leoemaxie/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gm.so
 export LD_PRELOAD=/tmp/gm.so
--- 0x18-dynamic_libraries/calculator.c
+++ 0x18-dynamic_libraries/calculator.c
@@ -1,21 +1,24 @@
 int mul(int a, int b)
 {
-	return a * b;
+	return (a * b);
 }
 
 int add(int a, int b)
 {
-	return a + b;
+	return (a + b);
 }
+
 int sub(int a, int b)
 {
-	return a - b;
+	return (a - b);
 }
+
 int div(int a, int b)
 {
-	return a / b;
+	return (a / b);
 }
+
 int mod(int a, int b)
 {
-	return a %  b;
+	return (a % b);
 }
